97e37f934da33b575e046cdb0f09659714286c7c
LeonardoFurtado <srleonardofurtado@gmail.com>
2017-05-25 10:52:15 -0300

    Inserção de funções

--- score.c
+++ score.c
@@ -1,58 +1,88 @@
-#include<Stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
 //prototype || protótipos
-int pushScore();
-void popScore();
-void insertScore();
+int getScore(int *array);
+void removeScore(int *array);
+void addScore(int *array);
 void printArray(int *array);
 void startArray(int *array);
+void printOptions();
+void getOption(int *array,int option);
 //End prototype || fim dos protótipos
-
 void main(){
 	int array[10];
+	int option=99;
 	startArray(array);
-	printArray(array);
+	while(option!=0){
+		printf("!-----Scores-----!\n");
+		printArray(array);
+		printOptions();
+		scanf("%d", &option);
+		getOption(array,option);
+		system("cls");
+	}
 }
-
 //functions || funções
-
-
-int pushScore(int *array,int position){
+void getOption(int *array,int option){
+	switch(option){
+			case 0:printf("Exiting!");
+							break;
+			case 1:addScore(array);
+							break;
+			case 2:removeScore(array);
+							break;
+			case 3:startArray(array);
+							break;
+			default:printf("Invalid");
+							break;
+	}
+}
+int getScore(int *array){
+	int position;
+	printf("insert a position:");
+	scanf("%d",&position);
 	return array[position-1];
 }
-void popScore(int *array, int position){
-	int i=position-1;
-	for(;(i<9);i++){
+void removeScore(int *array){
+	int position;
+	printf("insert a position:");
+	scanf("%d",&position);
+	int i;
+	for(i=position-1;(i<9);i++){
 		array[i]=array[i+1];
 	}
-	array[9]=NULL;
+	array[9]=0;
 }
-void insertScore(int *array, int value){
-	int i,aux;
-	if(value>array[9]){
-		array[9] = value;
-		for(i=9;i>0;i--){
-			if(array[i]>array[i-1]){
-				aux = array[i];
-				array[i] = array[i-1];
-				array[i-1] = aux;
-			}
-		}
-		printf("Sucess\n");
-	}else{
-		printf("Low score\n");
-		return;	
+void addScore(int *array){
+	int value;
+	printf("Insert Score:");
+	scanf("%d",&value);
+	int i;
+	if(value<=array[9]){
+			return;
 	}
+	for(i=9;(i>0)&&(value>array[i-1]);i--){
+			array[i] = array[i-1];
+	};
+	array[i]=value;
 }
 void printArray(int *array){
 	int i;
-	for(i=0;i<10;i++){		
-		printf("Score %d: %d\n",i+1,array[i]);
+	for(i=0;i<10;i++){
+			printf("|        %d\n",array[i] );
 	}
+	printf("!-----------------!\n\n\n");
 }
 void startArray(int *array){
 	int i;
 	for(i=0;i<10;i++){
-		array[i]=NULL;
+		array[i]=0;
 	}
-	printf("Array Start\n");
+}
+void printOptions(){
+    printf("Insert one option:\n");
+        printf("1 - Insert score \n");
+        printf("2 - Remove score\n");
+        printf("3 - Clear scores \n");
+        printf("0 - Exit \n");
 }
